4cd9df0c3cce9d750ae954564e3d15aacc698827
Johan Hansdes <johanhanses@gmail.com>
2022-11-27 20:51:07 +0100

    button transition

--- components/Button.vue
+++ components/Button.vue
@@ -10,7 +10,8 @@ defineProps<{
     :class="loading ? 'opacity-80 cursor-not-allowed' : ''"
   >
     <span
-      class="font-medium text-white bg-gray-900 flex justify-center items-center gap-2 rounded-md py-2 transition-all ease-in-out hover:opacity-80 duration-500 tracking-wider font-mono px-6 w-32 h-10"
+      class="font-medium text-white bg-gray-900 flex justify-center items-center gap-2 rounded-md py-2 transition-all ease-in-out hover:opacity-80 duration-500 tracking-wider font-mono h-10"
+      :class="loading ? 'px-4 w-14' : 'px-6 w-32'"
     >
       <slot />
     </span>
